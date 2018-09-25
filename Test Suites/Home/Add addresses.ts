c1b4ea07c650baf36373cb00a467454192d867d4
rodolphe <rodolphe.riviere@hotmail.fr>
2018-09-25 15:05:56 +0200

    pffffiiiooouuuu

--- Test Suites/Home/Add addresses.ts
+++ Test Suites/Home/Add addresses.ts
@@ -4,7 +4,7 @@
    <name>Add addresses</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-21T13:46:36</lastRun>
+   <lastRun>2018-09-25T15:03:12</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
